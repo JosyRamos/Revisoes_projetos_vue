9938b788da3a63eb49533291c81e58dc2b47b3af
JosyRamos <josileiaramos97@gmail.com>
2022-03-04 16:44:28 -0400

    atualizaçõe que não estão completas

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
-<div>
-  <h1>{{titulo}}</h1> {{/*ou pode ser usado o v-text="titulo" porem o a interpolação fica de melhor entendimento.*/}}
-
-<ul>
+<div class ="corpo">
+  <h1 class = "centralizado">{{titulo}}</h1> {{/*ou pode ser usado o v-text="titulo" porem o a interpolação fica de melhor entendimento.*/}}
+<table class = "tabela">
+  <ul class = "lista-fotos">
   <li v-for="foto of fotos">
-    <img :src=" foto.url " :alt="foto.titulos">{{/*ou pode ser usado :src que funciona do mesmo jeito*/}}
+    <img v-bind=" foto.url " v-bind:alt="foto.titulos">{{/*ou pode ser usado :src que funciona do mesmo jeito*/}}
   </li>
   </ul>
+
+</table>
+
   </div>
 </template>
 
@@ -15,21 +18,31 @@ export default {
   data(){
     return{
       titulo:'Cachorros de Várias cores',
-      fotos:[{
-         url:"https://img.freepik.com/fotos-gratis/cachorro-preto-grande_168440-185.jpg",
-        titulos:"Cachorro preto grande"
-      },
-      {
-         url:"https://static1.patasdacasa.com.br/articles/0/17/60/@/8421-pastor-branco-suico-se-apaixone-tambem-articles_media_mobile-2.jpg",
-        titulos:"Pastor Branco Suiço"
-      }
-     ]
+      fotos:[ ]
     }
+  },
+  created(){
+   let promise = this.$http.get('http://localhost:8080/v2/projetodaJosi');
+   promise.then(res  => {
+     res.json().then(fotos => this.fotos = fotos);{{/*pode ser usada de varias formas! porem é o mesmo resultado*/}}
+   });
+
   }
- 
+  
+
 }
 </script>
 
 <style>
+.corpo{
+  font-family:'Times New Roman', Times, serif; 
+  color: hotpink;
+  width:96%;
+  margin-top:0 auto;
+}
+.centralizado{
+color: darkslateblue;
+text-align: center;
+}
 
 </style>
